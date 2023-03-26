320c4ca502a8b2a83f7ab1e7312abbb7f3ba4f2a
jin <2293748571@qq.com>
2023-03-26 19:14:27 +0800

    添加控件

--- src/components/MapContainer/MapContainer.vue
+++ src/components/MapContainer/MapContainer.vue
@@ -17,6 +17,7 @@ export default {
         input: "",
       },
       auto: null,
+      placeSearch: null,
     };
   },
 
@@ -35,21 +36,42 @@ export default {
       AMapLoader.load({
         key: "2c1c4affeb410923990fec54c5af721a", // 申请好的Web端开发者Key，首次调用 load 时必填
         version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-        plugins: ["AMap.AutoComplete"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+        plugins: [
+          "AMap.ToolBar",
+          "AMap.Scale",
+          "AMap.HawkEye",
+          "AMap.MapType",
+          "AMap.Geolocation",
+          "AMap.AutoComplete",
+          "AMap.PlaceSearch",
+        ], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
       })
         .then((AMap) => {
           this.map = new AMap.Map("container", {
             resizeEnable: true,
             //设置地图容器id
             viewMode: "3D", //是否为3D地图模式
-            zoom: 10, //初始化地图级别
+            zoom: 13, //初始化地图级别
             center: [121.473667, 31.230525], //初始化地图中心点位置
           });
+          this.map.addControl(new AMap.Scale());
+          this.map.addControl(new AMap.ToolBar());
+          this.map.addControl(new AMap.HawkEye());
+          this.map.addControl(new AMap.MapType());
+          this.map.addControl(new AMap.Geolocation());
           this.auto = new AMap.AutoComplete(this.autoOptions);
+          this.placeSearch = new AMap.PlaceSearch({
+            map: this.map,
+          }); //构造地点查询类
+          this.auto.on("select", this.select);
         })
         .catch((e) => {
           console.log(e);
         });
+    },
+    select(e) {
+      this.placeSearch.setCity(e.poi.adcode);
+      this.placeSearch.search(e.poi.name); //关键字查询查询
     },
   },
 };
